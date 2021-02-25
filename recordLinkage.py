b367db53549ff4e470fe860712b75c6b503a9844
patrickrehill <lyrandar@gmail.com>
2021-02-25 16:01:57 +1100

    Packaged into main function

--- recordLinkage.py
+++ recordLinkage.py
@@ -24,6 +24,7 @@ import blocking
 import comparison
 import classification
 import evaluation
+import pandas as pd
 
 # =============================================================================
 # Variable names for loading datasets
@@ -52,239 +53,247 @@ truthfile_name = 'assignment-data/data_wrangling_rlgt.csv'
 rec_idA_col = 0
 rec_idB_col = 0
 
-# The list of attributes to be used either for blocking or linking
-#
-# For the example data sets used in COMP8430 data wrangling in 2020:
-# 
-#  0: rec_id
-#  1: first_name
-#  2: middle_name
-#  3: last_name
-#  4: gender
-#  5: current_age
-#  6: birth_date
-#  7: street_address
-#  8: suburb
-#  9: postcode
-# 10: state
-# 11: phone
-# 12: email
+def main(blocking_fn, classification_fn):
 
-attrA_list    = [1,2,3,4,6,7,8,9,10,11]
-attrB_list    = [1,2,3,4,6,7,8,9,10,11]
-
-# ******** In lab 3, explore different attribute sets for blocking ************
-
-# The list of attributes to use for blocking (all must occur in the above
-# attribute lists)
-#
-blocking_attrA_list = [4,7]
-blocking_attrB_list = [4,7]
-
-# ******** In lab 4, explore different comparison functions for different  ****
-# ********           attributes                                            ****
-
-# The list of tuples (comparison function, attribute number in record A,
-# attribute number in record B)
-#
-exact_comp_funct_list = [(comparison.exact_comp, 1, 1),  # First name
-                         (comparison.exact_comp, 2, 2),  # Middle name
-                         (comparison.exact_comp, 3, 3),  # Last name
-                         (comparison.exact_comp, 8, 8),  # Suburb
-                         (comparison.exact_comp,10,10),  # State
-                         ]
-
-approx_comp_funct_list = [(comparison.jaccard_comp, 1, 1),        # First name
-                          (comparison.dice_comp, 2, 2),           # Middle name
-                          (comparison.jaro_winkler_comp, 3, 3),   # Last name
-                          (comparison.bag_dist_sim_comp, 7, 7),   # Address
-                          (comparison.edit_dist_sim_comp, 8, 8),  # Suburb
-                          (comparison.exact_comp,10,10),          # State
-                         ]
-
-# =============================================================================
-#
-# Step 1: Load the two datasets from CSV files
-
-start_time = time.time()
-
-recA_dict = loadDataset.load_data_set(datasetA_name, rec_idA_col, \
-                                      attrA_list, headerA_line)
-recB_dict = loadDataset.load_data_set(datasetB_name, rec_idB_col, \
-                                      attrB_list, headerB_line)
-
-# Load data set of true matching pairs
-#
-true_match_set = loadDataset.load_truth_data(truthfile_name)
-
-loading_time = time.time() - start_time
-
-# -----------------------------------------------------------------------------
-# Step 2: Block the datasets
-
-def genericBlock (block_function = 'none', recA_dict = recA_dict, recB_dict = recB_dict, 
-                  blocking_attrA_list = blocking_attrA_list,
-                  blocking_attrB_list = blocking_attrB_list):
-
-    start_time = time.time()
-    
-    # Select one blocking technique
-    
-    if block_function == 'none':
-        # No blocking (all records in one block)
-        #
-        resultA = blocking.noBlocking(recA_dict)
-        resultB = blocking.noBlocking(recB_dict)
-
-    if block_function == 'attr':
-        # Simple attribute-based blocking
-        #
-        resultA = blocking.simpleBlocking(recA_dict, blocking_attrA_list)
-        resultB = blocking.simpleBlocking(recB_dict, blocking_attrB_list)
-
-    if block_function == 'soundex':
-        # Phonetic (Soundex) based blocking
-        #
-        resultA = blocking.phoneticBlocking(recA_dict, blocking_attrA_list)
-        resultB = blocking.phoneticBlocking(recB_dict, blocking_attrB_list)
-    
-    if block_function == 'slk':
-        # Statistical linkage key (SLK-581) based blocking
-        #
-        fam_name_attr_ind = 3
-        giv_name_attr_ind = 1
-        dob_attr_ind      = 6
-        gender_attr_ind   = 4
-        
-        resultA = blocking.slkBlocking(recA_dict, fam_name_attr_ind, \
-                                          giv_name_attr_ind, dob_attr_ind, \
-                                          gender_attr_ind)
-        resultB = blocking.slkBlocking(recB_dict, fam_name_attr_ind, \
-                                          giv_name_attr_ind, dob_attr_ind, \
-                                          gender_attr_ind)
-    
-    block_time = time.time() - start_time
-    
-    # Print blocking statistics
+    # The list of attributes to be used either for blocking or linking
     #
-    blocking.printBlockStatistics(resultA, resultB)
-    
-    return resultA, resultB, block_time
+    # For the example data sets used in COMP8430 data wrangling in 2020:
+    #
+    #  0: rec_id
+    #  1: first_name
+    #  2: middle_name
+    #  3: last_name
+    #  4: gender
+    #  5: current_age
+    #  6: birth_date
+    #  7: street_address
+    #  8: suburb
+    #  9: postcode
+    # 10: state
+    # 11: phone
+    # 12: email
 
-blockA_dict, blockB_dict, blocking_time = genericBlock(block_function='attr')
-# -----------------------------------------------------------------------------
-# Step 3: Compare the candidate pairs
+    attrA_list    = [1,2,3,4,6,7,8,9,10,11]
+    attrB_list    = [1,2,3,4,6,7,8,9,10,11]
 
-start_time = time.time()
+    # ******** In lab 3, explore different attribute sets for blocking ************
 
-sim_vec_dict = comparison.compareBlocks(blockA_dict, blockB_dict, \
-                                        recA_dict, recB_dict, \
-                                        approx_comp_funct_list)
+    # The list of attributes to use for blocking (all must occur in the above
+    # attribute lists)
+    #
+    blocking_attrA_list = [4,7]
+    blocking_attrB_list = [4,7]
 
-comparison_time = time.time() - start_time
+    # ******** In lab 4, explore different comparison functions for different  ****
+    # ********           attributes                                            ****
 
-# -----------------------------------------------------------------------------
-# Step 4: Classify the candidate pairs
+    # The list of tuples (comparison function, attribute number in record A,
+    # attribute number in record B)
+    #
+    exact_comp_funct_list = [(comparison.exact_comp, 1, 1),  # First name
+                             (comparison.exact_comp, 2, 2),  # Middle name
+                             (comparison.exact_comp, 3, 3),  # Last name
+                             (comparison.exact_comp, 8, 8),  # Suburb
+                             (comparison.exact_comp,10,10),  # State
+                             ]
 
-def genericClassification(classification_function = 'exact', sim_vec_dict = sim_vec_dict,
-                          sim_threshold = None, min_sim_threshold = None, 
-                          weight_vec = None, true_match_set = true_match_set):
+    approx_comp_funct_list = [(comparison.jaccard_comp, 1, 1),        # First name
+                              (comparison.dice_comp, 2, 2),           # Middle name
+                              (comparison.jaro_winkler_comp, 3, 3),   # Last name
+                              (comparison.bag_dist_sim_comp, 7, 7),   # Address
+                              (comparison.edit_dist_sim_comp, 8, 8),  # Suburb
+                              (comparison.exact_comp,10,10),          # State
+                             ]
+
+    # =============================================================================
+    #
+    # Step 1: Load the two datasets from CSV files
+
     start_time = time.time()
 
-    if classification_function == 'exact':
-        # Exact matching based classification
-        class_match_set1, class_nonmatch_set1 = \
-                     classification.exactClassify(sim_vec_dict)
+    recA_dict = loadDataset.load_data_set(datasetA_name, rec_idA_col, \
+                                          attrA_list, headerA_line)
+    recB_dict = loadDataset.load_data_set(datasetB_name, rec_idB_col, \
+                                          attrB_list, headerB_line)
 
-    if classification_function == 'simthresh':
-        # Similarity threshold based classification
+    # Load data set of true matching pairs
+    #
+    true_match_set = loadDataset.load_truth_data(truthfile_name)
+
+    loading_time = time.time() - start_time
+
+    # -----------------------------------------------------------------------------
+    # Step 2: Block the datasets
+
+    def genericBlock (block_function = 'none', recA_dict = recA_dict, recB_dict = recB_dict,
+                      blocking_attrA_list = blocking_attrA_list,
+                      blocking_attrB_list = blocking_attrB_list):
+
+        start_time = time.time()
+
+        # Select one blocking technique
+
+        if block_function == 'none':
+            # No blocking (all records in one block)
+            #
+            resultA = blocking.noBlocking(recA_dict)
+            resultB = blocking.noBlocking(recB_dict)
+
+        if block_function == 'attr':
+            # Simple attribute-based blocking
+            #
+            resultA = blocking.simpleBlocking(recA_dict, blocking_attrA_list)
+            resultB = blocking.simpleBlocking(recB_dict, blocking_attrB_list)
+
+        if block_function == 'soundex':
+            # Phonetic (Soundex) based blocking
+            #
+            resultA = blocking.phoneticBlocking(recA_dict, blocking_attrA_list)
+            resultB = blocking.phoneticBlocking(recB_dict, blocking_attrB_list)
+
+        if block_function == 'slk':
+            # Statistical linkage key (SLK-581) based blocking
+            #
+            fam_name_attr_ind = 3
+            giv_name_attr_ind = 1
+            dob_attr_ind      = 6
+            gender_attr_ind   = 4
+
+            resultA = blocking.slkBlocking(recA_dict, fam_name_attr_ind, \
+                                              giv_name_attr_ind, dob_attr_ind, \
+                                              gender_attr_ind)
+            resultB = blocking.slkBlocking(recB_dict, fam_name_attr_ind, \
+                                              giv_name_attr_ind, dob_attr_ind, \
+                                              gender_attr_ind)
+
+        block_time = time.time() - start_time
+
+        # Print blocking statistics
         #
-        class_match_set1, class_nonmatch_set1 = \
-                    classification.thresholdClassify(sim_vec_dict, sim_threshold)
-    
-    if classification_function == 'minsim':
-        # Minimum similarity threshold based classification
-        #
-        class_match_set1, class_nonmatch_set1 = \
-                    classification.minThresholdClassify(sim_vec_dict,
-                                                        min_sim_threshold)
-    
-    if classification_function == 'weightsim':
-        # Weighted similarity threshold based classification
-        #
-        # weight_vec = [1.0] * len(approx_comp_funct_list)
-    
-        # Lower weights for middle name and state
-        #
-        # weight_vec = [2.0, 1.0, 2.0, 2.0, 2.0, 1.0]
-    
-        class_match_set1, class_nonmatch_set1 = \
-                    classification.weightedSimilarityClassify(sim_vec_dict,
-                                                              weight_vec,
-                                                              sim_threshold)
-    
-    if classification_function == 'dt':
-        # A supervised decision tree classifier
-        # 
-        class_match_set1, class_nonmatch_set1 = \
-                  classification.supervisedMLClassify(sim_vec_dict, true_match_set)
+        blocking.printBlockStatistics(resultA, resultB)
 
-    class_time = time.time() - start_time
+        return resultA, resultB, block_time
 
-    return class_match_set1, class_nonmatch_set1, class_time
+    blockA_dict, blockB_dict, blocking_time = genericBlock(block_function=blocking_fn)
+    # -----------------------------------------------------------------------------
+    # Step 3: Compare the candidate pairs
 
-class_match_set, class_nonmatch_set, classification_time = genericClassification('dt')
+    start_time = time.time()
 
-# -----------------------------------------------------------------------------
-# Step 5: Evaluate the classification
+    sim_vec_dict = comparison.compareBlocks(blockA_dict, blockB_dict, \
+                                            recA_dict, recB_dict, \
+                                            approx_comp_funct_list)
 
-# Get the number of record pairs compared
-#
-num_comparisons = len(sim_vec_dict)
+    comparison_time = time.time() - start_time
 
-# Get the number of total record pairs to compared if no blocking used
-#
-all_comparisons = len(recA_dict) * len(recB_dict)
+    # -----------------------------------------------------------------------------
+    # Step 4: Classify the candidate pairs
 
-# Get the list of identifiers of the compared record pairs
-#
-cand_rec_id_pair_list = sim_vec_dict.keys()
+    def genericClassification(classification_function = 'exact', sim_vec_dict = sim_vec_dict,
+                              sim_threshold = None, min_sim_threshold = None,
+                              weight_vec = None, true_match_set = true_match_set):
+        start_time = time.time()
 
-# Blocking evaluation
-#
-rr = evaluation.reduction_ratio(num_comparisons, all_comparisons)
-pc = evaluation.pairs_completeness(cand_rec_id_pair_list, true_match_set)
-pq = evaluation.pairs_quality(cand_rec_id_pair_list, true_match_set)
+        if classification_function == 'exact':
+            # Exact matching based classification
+            class_match_set1, class_nonmatch_set1 = \
+                         classification.exactClassify(sim_vec_dict)
 
-print('Blocking evaluation:')
-print('  Reduction ratio:    %.3f' % (rr))
-print('  Pairs completeness: %.3f' % (pc))
-print('  Pairs quality:      %.3f' % (pq))
-print('')
+        if classification_function == 'simthresh':
+            # Similarity threshold based classification
+            #
+            class_match_set1, class_nonmatch_set1 = \
+                        classification.thresholdClassify(sim_vec_dict, sim_threshold)
 
-# Linkage evaluation
-#
-linkage_result = evaluation.confusion_matrix(class_match_set,
-                                             class_nonmatch_set,
-                                             true_match_set,
-                                             all_comparisons)
+        if classification_function == 'minsim':
+            # Minimum similarity threshold based classification
+            #
+            class_match_set1, class_nonmatch_set1 = \
+                        classification.minThresholdClassify(sim_vec_dict,
+                                                            min_sim_threshold)
 
-accuracy =    evaluation.accuracy(linkage_result)
-precision =   evaluation.precision(linkage_result)
-recall    =   evaluation.recall(linkage_result)
-fmeasure  =   evaluation.fmeasure(linkage_result)
+        if classification_function == 'weightsim':
+            # Weighted similarity threshold based classification
+            #
+            # weight_vec = [1.0] * len(approx_comp_funct_list)
 
-print('Linkage evaluation:')
-print('  Accuracy:    %.3f' % (accuracy))
-print('  Precision:   %.3f' % (precision))
-print('  Recall:      %.3f' % (recall))
-print('  F-measure:   %.3f' % (fmeasure))
-print('')
+            # Lower weights for middle name and state
+            #
+            # weight_vec = [2.0, 1.0, 2.0, 2.0, 2.0, 1.0]
 
-linkage_time = loading_time + blocking_time + comparison_time + \
-               classification_time
-print('Total runtime required for linkage: %.3f sec' % (linkage_time))
+            class_match_set1, class_nonmatch_set1 = \
+                        classification.weightedSimilarityClassify(sim_vec_dict,
+                                                                  weight_vec,
+                                                                  sim_threshold)
 
-# -----------------------------------------------------------------------------
+        if classification_function == 'dt':
+            # A supervised decision tree classifier
+            #
+            class_match_set1, class_nonmatch_set1 = \
+                      classification.supervisedMLClassify(sim_vec_dict, true_match_set)
 
-# End of program.
+        class_time = time.time() - start_time
+
+        return class_match_set1, class_nonmatch_set1, class_time
+
+    class_match_set, class_nonmatch_set, classification_time = genericClassification(classification_fn)
+
+    # -----------------------------------------------------------------------------
+    # Step 5: Evaluate the classification
+
+    # Get the number of record pairs compared
+    #
+    num_comparisons = len(sim_vec_dict)
+
+    # Get the number of total record pairs to compared if no blocking used
+    #
+    all_comparisons = len(recA_dict) * len(recB_dict)
+
+    # Get the list of identifiers of the compared record pairs
+    #
+    cand_rec_id_pair_list = sim_vec_dict.keys()
+
+    # Blocking evaluation
+    #
+    rr = evaluation.reduction_ratio(num_comparisons, all_comparisons)
+    pc = evaluation.pairs_completeness(cand_rec_id_pair_list, true_match_set)
+    pq = evaluation.pairs_quality(cand_rec_id_pair_list, true_match_set)
+
+    print('Blocking evaluation:')
+    print('  Reduction ratio:    %.3f' % (rr))
+    print('  Pairs completeness: %.3f' % (pc))
+    print('  Pairs quality:      %.3f' % (pq))
+    print('')
+
+    # Linkage evaluation
+    #
+    linkage_result = evaluation.confusion_matrix(class_match_set,
+                                                 class_nonmatch_set,
+                                                 true_match_set,
+                                                 all_comparisons)
+
+    accuracy =    evaluation.accuracy(linkage_result)
+    precision =   evaluation.precision(linkage_result)
+    recall    =   evaluation.recall(linkage_result)
+    fmeasure  =   evaluation.fmeasure(linkage_result)
+
+    print('Linkage evaluation:')
+    print('  Accuracy:    %.3f' % (accuracy))
+    print('  Precision:   %.3f' % (precision))
+    print('  Recall:      %.3f' % (recall))
+    print('  F-measure:   %.3f' % (fmeasure))
+    print('')
+
+    linkage_time = loading_time + blocking_time + comparison_time + \
+                   classification_time
+    print('Total runtime required for linkage: %.3f sec' % (linkage_time))
+
+    # Export blocking metrics
+
+    # -----------------------------------------------------------------------------
+
+    # End of program.
+
+
+main('soundex', 'dt')
+
